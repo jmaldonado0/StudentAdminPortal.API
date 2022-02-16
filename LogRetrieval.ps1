ba635006187ee62e2e6a1ef1c2d9612ea495f9e9
Luis Donate <luis.donate@nlsnow.com>
2022-02-16 16:38:21 -0400

    Made recommended changes

--- LogRetrieval.ps1
+++ LogRetrieval.ps1
@@ -1,11 +1,16 @@
 param(
-	$EndPointUrl,
-	$EndPointContainer,
-	$Sas,
+	$UriAddress,
 	#Optional Folder Name, for the logs
-	$FolderName,
+	$LogRetrievalFolderName,
 	$ScriptReturn = 0
 )
+
+$Script:UriSplit = $UriAddress.Split('?')
+
+if (!(IsValidURIAddress -address $Script:UriSplit[0]) -or !$Script:UriSplit[1]) {
+	"Invalid URI"
+	Exit 1
+}
 
 #TODO: Delete and replaced with just error, after testing is done.
 $EventLog = 'Error','Information', 'Warning'
@@ -13,7 +18,7 @@ $EventLog = 'Error','Information', 'Warning'
 #Logs sources to search.
 $LogSources = 'System'
 #TODO: Uncomment after testing is done
-#$LogSources = 'PARTY', 'CCS', 'EC', 'DCTAutoRetrySvcLog', 'DCTBillingMonitorLog', 'DCTMSMQBillingListener', 'DCTSchedRequestLog'
+#$LogSources = 'Application', 'PARTY', 'CCS', 'EC', 'DCTAutoRetrySvcLog', 'DCTBillingMonitorLog', 'DCTMSMQBillingListener', 'DCTSchedRequestLog'
 
 $Headers = @{
 	'x-ms-blob-type' = 'BlockBlob'
@@ -28,24 +33,34 @@ $DateSaveFormat = 'yyyy-MM-dd'
 foreach($logSource in $LogSources){
 	#TODO: Remove 'TESTLD1_'
 	#File naming concatenate format: Folder Name(if applied)/ServerName_logSource_Date.extension 
-	$file = (AddProperUriString -value $FolderName) + 'TESTLD1_' + $env:computername + '_' + $logSource + '_' + [DateTime]::Parse($DateToCheck).ToString($DateSaveFormat) + '.txt'
+	$local:file = (AddProperUriString -value $LogRetrievalFolderName) + 'TESTLD1_' + $env:computername + '_' + $logSource + '_' + [DateTime]::Parse($DateToCheck).ToString($DateSaveFormat) + '.txt'
 
-	$uri = $EndPointUrl + (AddProperUriString -value $EndPointContainer) + $file + $Sas
+	$local:uri = $Script:UriSplit[0] + $local:file + '?'+ $Script:UriSplit[1]
 
 	try { 
-		$logResult = Get-EventLog -LogName $logSource -EntryType $EventLog[0] -After ([DateTime]($DateToCheck)).AddDays(-1) -Before ([DateTime]($DateToCheck)) | ConvertTo-Csv | ConvertFrom-Csv | Out-String
+		#TODO: Maybe modify entry type to only accept 'x' log source
+		#$logResult = Get-EventLog -LogName $logSource -EntryType $EventLog[0] -After ([DateTime]($DateToCheck)).AddDays(-1) -Before ([DateTime]($DateToCheck)) | ConvertTo-Csv | ConvertFrom-Csv | Out-String
+		$logResult = Get-WinEvent -LogName $logSource| 
+		Select-Object * | 
+		Where-Object { 
+			$_.LevelDisplayName -eq $EventLog[0] -and 
+			($_.TimeCreated -gt ([DateTime]($DateToCheck)).AddDays(-1) -and	$_.TimeCreated -lt ([DateTime]($DateToCheck))) 
+			} | 
+		Out-String
 		
 		if($logResult) {
-			$result = Invoke-WebRequest -Uri $uri -Method Put -Headers $Headers -Body $logResult
+			$result = Invoke-WebRequest -Uri $local:uri -Method Put -Headers $Headers -Body $logResult
 			"Status code $($result.StatusCode) was executed for log source: $logSource"
 		}
 		else {
 			"File was empty for log source: $logSource"
+			$ScriptReturn = 1
 		}
 	}
 	catch { 
 		"On log source $logSource an exception was caught: $($_.Exception.Message)"
 		$_.Exception.Response
+		$ScriptReturn = 1
 	}
 }
 
@@ -56,6 +71,15 @@ function AddProperUriString {
 	IF ($value) {
 		return $value + '/'
 	}
+}
+
+function IsValidURIAddress {
+	param(
+		[string]$address
+	)
+	IF (!$address -or !$address.EndsWith('/')) { return $false }
+	$local:uri = $address -as [System.URI]
+	return ($null -ne $uri.AbsoluteURI -and $uri.Scheme -match '[http|https]')
 }
 
 Exit $ScriptReturn
--- LogRetrievalScheduleTask.ps1
+++ LogRetrievalScheduleTask.ps1
@@ -1,6 +1,6 @@
 $taskName = "LogRetrievalScheduleTask"
 $taskDescription = "Selects and Retrieves the Windows error event logs, and uploads it to a azure blob."
-$taskArgs = "-EndPointUrl #{endPointUrl} -EndPointContainer #{endPointContainer} -Sas #{sas}"
+$taskArgs = "-UriAddress #{uriAddress} -LogRetrievalFolderName #{logRetrievalFolderName}"
 $taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName}
 
 if($taskExists)
@@ -10,6 +10,6 @@ if($taskExists)
 }
 
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "#{deployPath}\LogRetrieval.ps1 $taskArgs"
-$trigger = New-ScheduledTaskTrigger -Daily -At 10:35am#-Daily -At 12:00am
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -TaskPath "#{Instance}\DuckCreek" -Description $taskDescription #-User '#{IdentUser}' -Password '#{IdentPass}'
-Disable-ScheduledTask $taskName -TaskPath "#{Instance}\DuckCreek"
+$trigger = New-ScheduledTaskTrigger -Daily -At 12:00am
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $taskDescription
+Disable-ScheduledTask $taskName 
