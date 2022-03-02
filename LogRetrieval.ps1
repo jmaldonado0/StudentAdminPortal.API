f6cba637ad2b5af825041bfbb23ea5c2cdce6573
Luis Donate <luis.donate@nlsnow.com>
2022-03-02 15:05:56 -0400

    Refactorying

--- LogRetrieval.ps1
+++ LogRetrieval.ps1
@@ -5,17 +5,17 @@ param(
 	$ScriptReturn = 0
 )
 
-function AddProperUriString {
+function AddProperUriString 
+{
 	param (
 		[string]$value
 	)
 
-	if ($value) {
-		return $value + '/'
-	}
+	if ($value) { return $value + '/' }
 }
 
-function IsValidURIAddress {
+function IsValidURIAddress 
+{
 	param(
 		[string]$address 
 	)
@@ -29,60 +29,67 @@ function IsValidURIAddress {
 
 if($null -eq $UriAddress)
 { 
-	"No URI has been provided."
+	Write-Error 'No URI has been provided.'
 	exit 1
 }
 
 $Script:UriSplit = $UriAddress.ToString().Split('?')
 
-if (!(IsValidURIAddress -address $Script:UriSplit[0]) -or !$Script:UriSplit[1]) {
-	"Invalid URI address"
-
+if (!(IsValidURIAddress -address $Script:UriSplit[0]) -or !$Script:UriSplit[1]) 
+{
+	Write-Error 'Invalid URI address'
 	Exit 1
 }
 
 #Logs sources to search.
 $LogSources = 'System', 'Application', 'PARTY', 'CCS', 'EC', 'DCTAutoRetrySvcLog', 'DCTBillingMonitorLog', 'DCTMSMQBillingListener', 'DCTSchedRequestLog'
 
-$Headers = @{
+$Headers = 
+@{
 	'x-ms-blob-type' = 'BlockBlob'
-	'x-ms-blob-content-type' = 'text/plain'
+	'x-ms-blob-content-type' = 'text/csv'
 }
 
-$Script:DateToCheck = Get-Date
+$Script:LogDate = Get-Date
+
+#LD = log date
+$Script:PreviousDayOfLD = $Script:LogDate.AddDays(-1)
 
 $DateSaveFormat = 'yyyy-MM-dd'
 
-"From date $(([DateTime]($Script:DateToCheck)).AddDays(-1)) to $Script:DateToCheck"
+"From date $Script:PreviousDayOfLD to $Script:LogDate"
 
-foreach($logSource in $LogSources){
+foreach($logSource in $LogSources)
+{
 	#File naming concatenation: Folder Name(if applied)/ServerName_logSource_Date.extension 
-	$local:file = (AddProperUriString -value $LogRetrievalFolderName) + ($env:computername, $logSource, [DateTime]::Parse($Script:DateToCheck).AddDays(-1).ToString($DateSaveFormat) -join '_') + '.csv'
+	$local:file = (AddProperUriString -value $LogRetrievalFolderName) + ($env:computername, $logSource, $Script:PreviousDayOfLD.ToString($DateSaveFormat) -join '_') + '.csv'
 	
 	$local:uri = $Script:UriSplit[0] + $local:file + '?' + $Script:UriSplit[1]
 
-	try {
+	try 
+	{
 		"Logging $logSource"
 
-		$logResult = Get-EventLog -LogName $logSource -After ([DateTime]($Script:DateToCheck)).AddDays(-1) -Before ([DateTime]($Script:DateToCheck)) | Sort-Object -Property TimeGenerated | ConvertTo-Csv 
+		$logResult = Get-EventLog -LogName $logSource -After $Script:PreviousDayOfLD -Before $Script:LogDate | Sort-Object -Property TimeGenerated | ConvertTo-Csv -NoTypeInformation | Out-String
 		
-		if($logResult) {
-			"Exporting log source: $logSource"
+		if($logResult) 
+		{
+			Write-Host "Attempting to export log source: $logSource"
 
 			$result = Invoke-WebRequest -Uri $local:uri -Method Put -Headers $Headers -Body $logResult
 
-			"Status code $($result.StatusCode) was executed for log source: $logSource`n"
+			Write-Host "Status code $($result.StatusCode) was executed for log source: $logSource`n" -ForegroundColor Green
 		}
 
-		else {
-			"No logs were found on log source: $logSource`n"
+		else 
+		{
+			Write-Warning "No logs were found on log source: $logSource`n" 
 		}
 	}
 
-	catch { 
-		"On log source $logSource an exception was caught: $($_.Exception.Message)"
-
-		$_.Exception.Response + "`n"
+	catch 
+	{ 
+		Write-Warning "On log source $logSource an exception was caught:`n$($_.Exception.Message)`n"
 
 		$ScriptReturn = 1
 	}
